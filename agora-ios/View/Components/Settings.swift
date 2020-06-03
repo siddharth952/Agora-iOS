b9fdc6ae2887e07665c0f4b49917d337b865c249
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-06-03 10:09:48 +0530

    Add more options in Settings View

--- agora-ios/View/Components/Settings.swift
+++ agora-ios/View/Components/Settings.swift
@@ -12,14 +12,12 @@ struct Settings: View {
     @State private var showingAlert = false
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack() {
             SettingsTop()
-            Text("Settings").font(.title).fontWeight(.bold).padding(.leading,20)
-            Divider()
-            Spacer()
-            
+            Text("Settings").font(.title).fontWeight(.bold)
+            Divider().offset(y:4)
             ZStack {
-                Color.black.opacity(0.03).cornerRadius(10)
+                Color.black.opacity(0.04)
                 VStack(alignment:.leading,spacing: 20){
                     Text("Manage Data").fontWeight(.bold)
                     Divider()
@@ -28,35 +26,48 @@ struct Settings: View {
                     Text("Rate Us").fontWeight(.bold)
                     Divider()
                     Text("Contact Us").fontWeight(.bold)
-                }.padding()
+                    Divider()
+                    Button(action: {
+                        self.showingAlert = true;
+                        
+                    }) {
+                        Text("Logout")
+                    }.alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Log out?"), message: Text("Are you sure you want to log out?"),primaryButton: .default(Text("Yes"), action: {
+                            print("Logging out...")
+                            
+                            
+                            UserDefaults.standard.set(false, forKey: "status")
+                            NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+                            
+                        }), secondaryButton: .default(Text("Dismiss")))
+                    }
+                }.padding(.leading,20)
             }
-            
-            Button(action: {
-                       // self.showingAlert = true;
-
-                     UserDefaults.standard.set(false, forKey: "status")
-                     NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
-                       }) {
-                           Text("Logout").padding(.leading,20)
-                           }
+            Spacer()
         }
     }
 }
 
 struct SettingsTop:View{
-    @State var name = ""
-    @State var email = ""
+    @State var name = "Name"
+    @State var email = "xyz@gmail.com"
     var body : some View{
         
         VStack(spacing:0){
             ZStack(){
                     Image("Mountains")
                 HStack(){
-                    Circle().frame(width:64,height:64).shadow(radius: 1)
+                    Circle()
+                        .stroke(LinearGradient(gradient: Gradient(colors: [Color("Color2"), Color("Color1")]), startPoint: .top, endPoint: .bottom), lineWidth: 4)
+                        .frame(width:64,height:64)
+                        .background(Text("SS").font(.title).fontWeight(.bold))
+
+                    Text(self.name + "\n" + self.email)
                     Spacer()
                 }.padding()
             }.padding(.top,UIApplication.shared.windows.first?.safeAreaInsets.top)
-        }.edgesIgnoringSafeArea(.top)
+        }.edgesIgnoringSafeArea(.top).padding(.leading,20)
     }
 }
 
@@ -64,6 +75,14 @@ struct SettingsTop:View{
 
 struct Settings_Previews: PreviewProvider {
     static var previews: some View {
-        Settings()
+        Group {
+                       Settings()
+                          .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+                          .previewDisplayName("iPhone 8")
+
+                       Settings()
+                          .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+                          .previewDisplayName("iPhone 11")
+                    }
     }
 }
