1405bad4a1bdf8bb9cd105cc23ff2003e5f8c297
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-06-16 16:46:07 +0530

    Modify GraphQL  parameters

--- agora-ios/Utils/UserLoginManager.swift
+++ agora-ios/Utils/UserLoginManager.swift
@@ -9,10 +9,12 @@ import Foundation
 import SwiftUI
 import FacebookLogin
 
+
+
 class UserLoginManager: ObservableObject {
     let loginManager = LoginManager()
     func facebookLogin() {
-        loginManager.logIn(permissions: [.publicProfile, .email], viewController: nil) { loginResult in
+        loginManager.logIn(permissions: [.publicProfile, .email,.publicProfile], viewController: nil) { loginResult in
             switch loginResult {
             case .failed(let error):
                 print(error)
@@ -20,7 +22,7 @@ class UserLoginManager: ObservableObject {
                 print("User cancelled login.")
             case .success(let grantedPermissions, let declinedPermissions, let accessToken):
                 print("Logged in! \(grantedPermissions) \(declinedPermissions) \(accessToken)")
-                GraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name"]).start(completionHandler: { (connection, result, error) -> Void in
+                GraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture"]).start(completionHandler: { (connection, result, error) -> Void in
                     if (error == nil){
                         let fbDetails = result as! NSDictionary
                         print(fbDetails)
