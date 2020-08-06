d11907799036fcdb09d2a0e8aac95a72b3395ab9
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-08-06 09:41:39 +0530

    Add Details

--- agora-ios/View/Components/ElectionDetail/ElectionDetailView.swift
+++ agora-ios/View/Components/ElectionDetail/ElectionDetailView.swift
@@ -9,23 +9,22 @@
 import SwiftUI
 
 struct ElectionDetailsView: View {
-    @State var showResultsView:Bool
-    var electionName:String = "Melbourne March Election"
-    var electionAlgo:String = ""
+    @ObservedObject var calendarManager:CalendarManager
+    
     
     var body: some View {
         
-        return VStack(alignment: .leading) {
+        return VStack() {
             ZStack {
                 HStack(spacing:10) {
                     
-                    Button(action: { self.showResultsView.toggle() }
+                    Button(action: { self.calendarManager.eventUpdateOverlayShow.toggle() }
                     ){
-                        Image(systemName: "chevron.left").font(.title)
+                        Image(systemName: "multiply.circle.fill").font(.title)
                     }.foregroundColor(.white)
                     Spacer()
                 }.background(LinearGradient(gradient: Gradient(colors: [Color("Color2_2"), Color("Color2")]), startPoint: .bottom, endPoint: .top).frame(width: UIScreen.main.bounds.width , height: UIScreen.main.bounds.height / 6 , alignment: .center).edgesIgnoringSafeArea(.top))
-                Text(electionName)
+                Text(calendarManager.election[0].name)
                     .foregroundColor(.white)
                     .fontWeight(.regular)
                     .font(.title)
@@ -36,21 +35,59 @@ struct ElectionDetailsView: View {
                 Button(action: {}){
                     Text("Show Election Results").frame(width: UIScreen.main.bounds.width / 1.4 ,height: 50).foregroundColor(.white)
                     
-                }.foregroundColor(.black)
-                    .background(LinearGradient(gradient: Gradient(colors: [Color("Color2_2"), Color("Color2")]), startPoint: .bottom, endPoint: .top))
-                    .cornerRadius(20)
-                    .navigationBarTitle("")
-                    .navigationBarHidden(true)
-                    .navigationBarBackButtonHidden(true)
+                }.customButton(width: UIScreen.main.bounds.width / 1.4)
+                
+                Spacer()
+            }.padding(.top,20).padding(.bottom,5)
+            
+            HStack {
+                Spacer()
+                Button(action: {}){
+                    Text("Remind Me").frame(width: UIScreen.main.bounds.width / 1.4 ,height: 50).foregroundColor(.white)
+                    
+                }.customButton(width: UIScreen.main.bounds.width / 1.4)
+                
                 Spacer()
             }
             
             Divider()
             
-            VStack {
-                Text("Election Name: " + electionName)
-                Text("Algorithm: " + electionAlgo)
-            }.padding()
+            VStack(alignment:.leading) {
+                Group{
+                    Text("Election Name: " + calendarManager.election[0].name)
+                    .fontWeight(.medium)
+                        .font(.footnote)
+                    .padding(.top,10)
+                    .padding(.bottom,10)
+                    Text("Election description: " + calendarManager.election[0].description)
+                    .fontWeight(.medium)
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    .padding(.top,10)
+                    .padding(.bottom,10)
+                    Text("Algorithm: " + calendarManager.election[0].votingAlgo)
+                    .fontWeight(.medium)
+                    .font(.footnote)
+                    .padding(.top,10)
+                    .padding(.bottom,10)
+                    Text("Start : " + calendarManager.election[0].startingDate + "\nEnd : "+calendarManager.election[0].endingDate)
+                    .fontWeight(.medium)
+                    .font(.footnote)
+                    .padding(.top,10)
+                    .padding(.bottom,10)
+                    Text("Ballots: " + "\(calendarManager.election[0].ballot.count)")
+                    .fontWeight(.medium)
+                    .font(.footnote)
+                    .padding(.top,10)
+                    .padding(.bottom,10)
+                   
+                }
+            }.padding(.top, 10)
+            .padding(.leading,10)
+            .frame(width:UIScreen.main.bounds.width - 20)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .animation(.spring())
             
             Spacer()
             
@@ -60,6 +97,6 @@ struct ElectionDetailsView: View {
 
 struct ElectionDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        ElectionDetailsView(showResultsView: true)
+        ElectionDetailsView(calendarManager: CalendarManager())
     }
 }
